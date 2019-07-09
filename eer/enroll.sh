618a168f536a0ae048704ddebc5f4fb4c16ae86d
linlinsongyun <37888350+linlinsongyun@users.noreply.github.com>
2019-07-09 20:33:32 +0800

    Create enroll.sh

---
+++ eer/enroll.sh
@@ -0,0 +1,97 @@
+. ./cmd.sh
+. ./path.sh
+set -e
+
+if [ $# != 2 ] ; then
+        echo "USAGE: $0 wav_path(or speaker_path) type"
+        exit 1;
+fi
+
+if [[ "$2" = "1" ]] ; then
+        wavdir=$1
+        DataPre=1
+        echo "1: without speaker"
+elif [[ "$2" = "2" ]] ; then
+        speakerPath=$1
+        DataPre=2
+        echo "2: with speaker"
+else
+        echo "Error: type should set to be 1 or 2"
+        exit 1;
+fi
+FIXDATA=1
+FeatureForMfcc=1
+VAD=1
+EXTRACT=1
+DOLDA=1
+plda=1
+datadir=`pwd`/data/enroll
+logdir=`pwd`/data/enroll/log
+featdir=`pwd`/data/enroll/feat
+nnet_dir=`pwd`/exp/xvector_nnet_1a
+kaldi_dir=/opt/kaldi
+if [ $DataPre -eq 1 ]; then
+        echo ==========================================
+        echo "get utt2spk, DataPre start on" `date`
+        echo ==========================================
+
+        python make_data.py $wavdir $datadir
+        utils/utt2spk_to_spk2utt.pl $datadir/utt2spk > $datadir/spk2utt || exit 1
+        utils/spk2utt_to_utt2spk.pl $datadir/spk2utt > $datadir/utt2spk || exit 1
+
+        echo ===== data preparatin finished successfully `date`==========
+else
+        echo ==========================================
+        echo "get utt2spk, DataPre start on" `date`
+        echo ==========================================
+        python make_data_speaker.py $speakerPath $datadir
+        $kaldi_dir/egs/timit/s5/utils/utt2spk_to_spk2utt.pl $datadir/utt2spk > $datadir/spk2utt || exit 1
+        $kaldi_dir/egs/timit/s5/utils/spk2utt_to_utt2spk.pl $datadir/spk2utt > $datadir/utt2spk || exit 1
+
+        echo ===== data preparatin finished successfully `date`==========
+fi
+
+if [ $FIXDATA -eq 1 ]; then
+    echo ==========================================
+        echo "sorted spk2utt ... : fix_data_dir start on" `date`
+        echo ==========================================
+    $kaldi_dir/egs/timit/s5/utils/fix_data_dir.sh $datadir
+        echo ====== fix_data_dir finished successfully `date` ==========
+fi
+
+
+if [ $FeatureForMfcc -eq 1 ]; then
+         echo ==========================================
+         echo "FeatureForSpeaker start on" `date`
+         echo ==========================================
+        # Extract speaker features MFCC.
+    steps/make_mfcc.sh --mfcc-config conf/mfcc.conf --nj 1 --cmd "$train_cmd" \
+    $datadir $logdir/make_enrollmfcc $featdir/mfcc
+
+    $kaldi_dir/egs/timit/s5/utils/fix_data_dir.sh $datadir
+        echo ==== FeatureForSpeaker test successfully `date` ===========
+
+fi
+if [ $VAD -eq 1 ];then
+         echo ==========================================
+         echo "generate vad file in data/train, VAD start on" `date`
+         echo ==========================================
+        # Compute VAD decisions. These will be shared across both sets of features.
+        sid/compute_vad_decision.sh --nj 1 --cmd "$train_cmd" \
+    $datadir $logdir/make_enrollvad $featdir/vad
+
+        utils/fix_data_dir.sh $datadir
+
+echo ========== VAD test successfully `date` ===============
+fi
+
+if [ $EXTRACT -eq 1 ]; then
+         echo ==========================================
+         echo "EXTRACT start on" `date`
+         echo ==========================================
+        # Extract the xVectors
+        sid/nnet3/xvector/extract_xvectors.sh --cmd "$train_cmd" --nj 1 \
+    $nnet_dir $datadir $featdir/xvectors_enroll_mfcc
+
+        echo ========= EXTRACT just for testing `date`=============
+fi
