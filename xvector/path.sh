090fa0dd1906c17427abe8f2363bcbf976af662e
linlinsongyun <37888350+linlinsongyun@users.noreply.github.com>
2020-01-02 13:20:59 +0800

    Update path.sh

--- xvector/path.sh
+++ xvector/path.sh
@@ -1,4 +1,4 @@
-export KALDI_ROOT=`pwd`/../../kaldi-master
+export KALDI_ROOT=/opt/kaldi
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sph2pipe_v2.5:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
@@ -6,6 +6,6 @@ export LC_ALL=C
 
 
 # If there is no utils in the dictionary, then add link to original kaldi
-# ln -s $KALDI_ROOT/egs/sre16/v2/steps ./
-# ln -s $KALDI_ROOT/egs/sre16/v2/sid ./
-# ln -s $KALDI_ROOT/egs/sre16/v2/utils ./
+ln -s $KALDI_ROOT/egs/sre16/v2/steps ./
+ln -s $KALDI_ROOT/egs/sre16/v2/sid ./
+ln -s $KALDI_ROOT/egs/sre16/v2/utils ./
